79fda0411b55e8111d6a44ee4d60741281df6d2d
RawanElbanna <99205603+RawanElbanna@users.noreply.github.com>
2022-04-29 06:31:34 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -1,1 +1,12 @@
-
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 100) {
+//           $(".navbar").css("background" , "colur");
+//         }
+  
+//         else{
+//             $(".navbar").css("background" , "transparent");  	
+//         }
+//     })
+//   })
